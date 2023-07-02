997a1f1e4e4dfed5c091249c62721e9cb8254d23
rjames187 <rory.james2021@gmail.com>
2023-07-01 23:48:51 -0500

    write get urls from html func

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require('jsdom')
+
 function normalizeURL (url) {
   const parsedURL = new URL(url)
   const { pathname } = parsedURL
@@ -6,6 +8,18 @@ function normalizeURL (url) {
   return parsedURL.toString()
 }
 
+function getURLsFromHTML (htmlBody, baseURL) {
+  const dom = new JSDOM(htmlBody)
+  const anchors = Array.from(dom.window.document.querySelectorAll('a'))
+  const res = anchors.map((a) => {
+    let { href } = a
+    if (href.charAt(0) === '/') href = `${baseURL}${href}`
+    return href
+  })
+  return res
+}
+
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,7 +1,27 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 
 const NORMAL_URL = 'http://wagslane.dev/path'
+
+const ONE_LINK_HTML = `<html>
+  <body>
+      <a href="https://blog.boot.dev/funny"><span>Go to Boot.dev</span></a>
+  </body>
+  </html>`
+
+const THREE_LINK_HTML = `<html>
+  <body>
+      <a href="https://blog.boot.dev/funny"><span>Go to Boot.dev</span></a>
+      <a href="https://blog.boot.dev/funny"><span>Go to Boot.dev</span></a>
+      <a href="https://blog.boot.dev/funny"><span>Go to Boot.dev</span></a>
+  </body>
+  </html>`
+
+const REL_LINK_HTML = `<html>
+<body>
+    <a href="/blog"><span>Go to Boot.dev</span></a>
+</body>
+</html>`
 
 test('a slash at the end of a url is removed', () => {
   const res = normalizeURL('http://wagslane.dev/path/')
@@ -21,4 +41,22 @@ test('url to converted to lower case', () => {
 test('multiple aspects can be normalized at once', () => {
   const res = normalizeURL('https://WagsLane.DEV/path/')
   expect(res).toEqual(NORMAL_URL)
+})
+
+test('extracts a link from html', () => {
+  const links = getURLsFromHTML(ONE_LINK_HTML, 'https://blog.boot.dev')
+  expect(links[0]).toEqual('https://blog.boot.dev/funny')
+})
+
+test('extracts multiple links from html', () => {
+  const links = getURLsFromHTML(THREE_LINK_HTML, 'https://blog.boot.dev')
+  expect(links).toHaveLength(3)
+  for (const link of links) {
+    expect(link).toEqual('https://blog.boot.dev/funny')
+  }
+})
+
+test('expect relative urls to be populated with base url', () => {
+  const links = getURLsFromHTML(REL_LINK_HTML, 'https://blog.boot.dev')
+  expect(links[0]).toEqual('https://blog.boot.dev/blog')
 })
